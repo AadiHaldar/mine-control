55dfd8a1ca3add83013ebc550da6b9cb4abe6074
AadiHaldar <aadi.haldar2006@gmail.com>
2026-03-04 22:05:33 +0530

    Change database URL to use temporary directory

--- server/database.py
+++ server/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./local_mine.db"
+DATABASE_URL = "sqlite:////tmp/local_mine.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
